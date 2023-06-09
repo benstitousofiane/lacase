20ba8c1c55cab322a9cc8737b3d8f8f120dde9c4
Benstitou Sofiane <129552238+benstitousofiane@users.noreply.github.com>
2023-06-09 13:26:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 // Développé par Benstitou Sofiane
 function mathDisplay(){
   let mathInput = document.getElementById("mathInput").value;
+  let textInput = document.getElementById("textInput").value;
   let mathOutput = document.getElementById("mathOutput");
+  let textOutput = document.getElementById("textOutput");
+  
+  textOutput.innerHTML = textInput;
   katex.render(mathInput, mathOutput, {
     throwOnError: false,
   });
