8cc59a7098c363fd3d9ab88f5628e54feb60a372
Benstitou Sofiane <129552238+benstitousofiane@users.noreply.github.com>
2023-06-22 13:32:25 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,141 +1,75 @@
-// Développée par Benstitou Sofiane
-
-lolContainer = document.getElementById('lol');
-navContainer = document.getElementById('nav');
-articleContainer  = document.getElementById('article');
-addTextButton = document.getElementById('addTextButton');
-addMathButton = document.getElementById('addMathButton');
-addImageButton = document.getElementById('addImageButton');
+let lol = document.getElementById('lol');
+let lacase = document.getElementById('lacase');
+let displayer = document.getElementById('displayer');
+let nav = document.getElementById('nav');
 
 
 function addText(){
-    navContainer.removeChild(addTextButton);
-    navContainer.removeChild(addMathButton);
-    navContainer.removeChild(addImageButton);
+    let newCaseInput = document.createElement('textarea');
+    newCaseInput.setAttribute('class', 'textInput')
+    
+    let newLaButton = document.createElement('button');
+    newLaButton.setAttribute('class', 'purple');
+    newLaButton.innerText = 'La';
+    
+    displayer.appendChild(newCaseInput);
+    displayer.appendChild(newLaButton);
 
-    newTextCase = document.createElement('textarea');
-    newTextCase.setAttribute('class', 'textCase');
-    newTextCase.setAttribute('cols', '50');
-    newTextCase.setAttribute('rows', '2');
+    newCaseInput.focus()
 
-
-    LaButton = document.createElement('button');
-    LaButton.innerText = "La"
-    LaButton.setAttribute('class', 'purple');
-
-    lolContainer.appendChild(newTextCase);
-    lolContainer.appendChild(LaButton);
-    newTextCase.focus();
-
-    LaButton.addEventListener('click', function(){
-        textDisplay();
-        textOutput.addEventListener('dblclick', function(){
-            articleContainer.removeChild(textOutput);
-        })
+    newLaButton.addEventListener('click', function(){
+        let newTextElement = document.createElement('p');
+        newTextElement.setAttribute('class', 'textOutput')
+        newTextElement.innerText = newCaseInput.value;
+        
+        displayer.removeChild(newCaseInput);
+        displayer.removeChild(newLaButton);
+        
+        lacase.appendChild(newTextElement);
+        newTextElement.addEventListener('dblclick', function(){
+            lacase.removeChild(newTextElement);
+        });
     });
+    
 }
-
-function textDisplay(){
-    textOutput =  document.createElement('p');
-    textOutput.innerText = newTextCase.value;
-    textOutput.setAttribute('class', 'textOutput');
-
-    addTextButton = document.createElement('button');
-    addTextButton.innerText = '+T'
-    addTextButton.setAttribute('class', 'orange');
-    addTextButton.setAttribute('onclick', 'addText()');
-
-
-    addMathButton = document.createElement('button');
-    addMathButton.innerText = '+M'
-    addMathButton.setAttribute('class', 'green');
-    addMathButton.setAttribute('onclick', 'addMath()');
-
-    addImageButton = document.createElement('button');
-    addImageButton.innerText = '+I'
-    addImageButton.setAttribute('class', 'blue');
-    addImageButton.setAttribute('onclick', 'addImage()');
-
-    lolContainer.removeChild(newTextCase);
-    lolContainer.removeChild(LaButton);
-
-    articleContainer.appendChild(textOutput);
-
-    navContainer.appendChild(addTextButton);
-    navContainer.appendChild(addMathButton);
-    navContainer.appendChild(addImageButton);
-}
-
 
 function addMath(){
-    navContainer.removeChild(addTextButton);
-    navContainer.removeChild(addMathButton);
-    navContainer.removeChild(addImageButton);
-
-    newMathCase = document.createElement('textarea');
-    newMathCase.setAttribute('class', 'mathCase');
-
-    LaButton = document.createElement('button');
-    LaButton.innerText = "La"
-    LaButton.setAttribute('class', 'purple');
+    let newCaseInput = document.createElement('textarea');
+    newCaseInput.setAttribute('class', 'mathInput')
     
+    let newLaButton = document.createElement('button');
+    newLaButton.setAttribute('class', 'purple');
+    newLaButton.innerText = 'La';
+    
+    displayer.appendChild(newCaseInput);
+    displayer.appendChild(newLaButton);
 
-    lolContainer.appendChild(newMathCase);
-    lolContainer.appendChild(LaButton);
+    newCaseInput.focus()
 
-    newMathCase.focus()
+    newLaButton.addEventListener('click', function(){
+        let newMathElement = document.createElement('p');
+        newMathElement.setAttribute('class', 'textOutput')
+        
+        katex.render(newCaseInput.value, newMathElement, {
+            throwOnError : false,
+        });
 
-    LaButton.addEventListener('click', function(){
-        mathDisplay();
-        mathOutput.addEventListener('dblclick', function(){
-            articleContainer.removeChild(mathOutput);
-        })
+        displayer.removeChild(newCaseInput);
+        displayer.removeChild(newLaButton);
+        
+        lacase.appendChild(newMathElement);
+        newMathElement.addEventListener('dblclick', function(){
+            lacase.removeChild(newMathElement);
+        });
     });
+    
 }
-
-function mathDisplay(){
-    mathOutput = document.createElement('p')
-    //Mettre sous format maths en sortie avec KaTeX
-    katex.render(newMathCase.value, mathOutput, {
-        throwOnError : false,
-    });
-    mathOutput.setAttribute('class', 'mathOutput');
-    mathOutput.setAttribute('cols', '50');
-    mathOutput.setAttribute('rows', '2');
-
-    addTextButton = document.createElement('button');
-    addTextButton.innerText = '+T'
-    addTextButton.setAttribute('class', 'orange');
-    addTextButton.setAttribute('onclick', 'addText()');
-
-
-    addMathButton = document.createElement('button');
-    addMathButton.innerText = '+M'
-    addMathButton.setAttribute('class', 'green');
-    addMathButton.setAttribute('onclick', 'addMath()');
-
-    addImageButton = document.createElement('button');
-    addImageButton.innerText = '+I'
-    addImageButton.setAttribute('class', 'blue');
-    addImageButton.setAttribute('onclick', 'addImage()');
-
-
-    lolContainer.removeChild(newMathCase);
-    lolContainer.removeChild(LaButton);
-
-    articleContainer.appendChild(mathOutput);
-
-    navContainer.appendChild(addTextButton);
-    navContainer.appendChild(addMathButton);
-    navContainer.appendChild(addImageButton);
-}
-
-
 
 //a faire :
 /*
--suppression des case et élément (édition si possible)
--lignes plus grandes.
+-suppression des case et élément (édition si possible) -> FAIT
+-lignes plus grandes. -> Fait
 -raccourci pour écrire et executer plus vite comme sur jupyter...
 -avoir une copie du text mis avant dans le presse papier
+- faire si possible des macros.
 */
